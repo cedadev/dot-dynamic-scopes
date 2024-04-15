a25519580f0f79a38e66a1d7c6fcc60eefbc4fb1
Alex Manning <alexander.manning@stfc.ac.uk>
2024-04-15 16:21:28 +0100

    Cache scopes for faster queries.

--- dot_dynamic_scopes/scopes.py
+++ dot_dynamic_scopes/scopes.py
@@ -1,24 +1,28 @@
-"""
-Django OAuth Toolkit scopes backend for the dot-dynamic-scopes package.
-"""
+"""Django OAuth Toolkit scopes backend for the dot-dynamic-scopes package."""
 
-from django.conf import settings
-from django.utils import module_loading
-
+import django.core.cache
 from oauth2_provider.scopes import BaseScopes
 
 from .models import Scope
 
 
 class DynamicScopes(BaseScopes):
-    """
-    Scopes backend that provides scopes from a Django model.
-    """
-    def get_all_scopes(self):
-        return { scope.name: scope.description for scope in Scope.objects.all() }
+    """Scopes backend that provides scopes from a Django model."""
 
-    def get_available_scopes(self, application = None, request = None, *args, **kwargs):
+    @staticmethod
+    def _get_scopes():
+        return {scope.name: scope.description for scope in Scope.objects.all()}
+
+    def get_all_scopes(self):
+        """Get all scopes."""
+        return django.core.cache.cache.get_or_set(
+            "dot-dynamic-scopes-scopes", self._get_scopes, 30
+        )
+
+    def get_available_scopes(self, application=None, request=None, *args, **kwargs):
+        """Get only scopes valid for this application and request."""
         return list(self.get_all_scopes().keys())
 
-    def get_default_scopes(self, application = None, request = None, *args, **kwargs):
-        return [scope.name for scope in Scope.objects.filter(is_default = True)]
+    def get_default_scopes(self, application=None, request=None, *args, **kwargs):
+        """Get scopes which are added by default."""
+        return [scope.name for scope in Scope.objects.filter(is_default=True)]
