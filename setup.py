1e5ee66f744fca8895b40801506c03e3fe4dc79a
Alex Manning <alexander.manning@stfc.ac.uk>
2024-04-15 16:30:58 +0100

    Update packaging.

--- setup.py
+++ setup.py
@@ -1,35 +1,3 @@
-#!/usr/bin/env python3
+import setuptools
 
-import os
-from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, 'README.md')) as f:
-    README = f.read()
-
-if __name__ == "__main__":
-    setup(
-        name = 'dot-dynamic-scopes',
-        version = "1.0.0",
-        description = 'Django OAuth Toolkit extension to allow scopes to be provided by a Django model.',
-        long_description = README,
-        classifiers = [
-            "Programming Language :: Python",
-            "Framework :: Django",
-            "Topic :: Internet :: WWW/HTTP",
-            "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-        author = 'Matt Pryor',
-        author_email = 'matt.pryor@stfc.ac.uk',
-        url = 'https://github.com/cedadev/dot-dynamic-scopes',
-        keywords = 'django oauth dynamic resource scopes',
-        packages = find_packages(),
-        include_package_data = True,
-        zip_safe = False,
-        install_requires = [
-            'django',
-            'django-oauth-toolkit',
-            'django-settings-object',
-        ],
-    )
+setuptools.setup()
